7dd5ffca706eafddb548d7400281589b146c8a29
PROSENJIT\Prosenjit <prosenjitmalda640@gmail.com>
2022-07-08 22:16:38 +0530

    change main method

--- Java/Generics_java/Hello.java
+++ Java/Generics_java/Hello.java
@@ -1,7 +1,7 @@
 package Java.Generics_java;
 
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String... java) {
         System.out.println("Hello world");
     }
 }
